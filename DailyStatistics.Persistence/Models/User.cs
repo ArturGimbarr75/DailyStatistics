10d3272b41d2124835ab3e3a074ca0cb03a40bdf
ArturGimbarr75 <47144232+ArturGimbarr75@users.noreply.github.com>
2024-05-25 19:35:08 +0300

    Add User model and identity

---
+++ DailyStatistics.Persistence/Models/User.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace DailyStatistics.Persistence.Models;
+
+public sealed class User : IdentityUser
+{
+	public string Name { get; set; } = default!;
+	public string Surname { get; set; } = default!;
+}
