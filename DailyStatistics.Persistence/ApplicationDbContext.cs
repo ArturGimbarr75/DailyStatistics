5c7f824ab84a583517fdab24e914d5eabad43685
ArturGimbarr75 <47144232+ArturGimbarr75@users.noreply.github.com>
2024-05-27 19:30:43 +0300

    Add DbContext and User model

---
+++ DailyStatistics.Persistence/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using DailyStatistics.Persistence.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DailyStatistics.Persistence;
+
+public sealed class ApplicationDbContext : DbContext
+{
+	public DbSet<User> Users { get; set; } = default!;
+
+	public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+	{
+
+	}
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		base.OnModelCreating(modelBuilder);
+		modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+	}
+}
+
