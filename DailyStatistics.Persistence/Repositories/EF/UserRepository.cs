34f4d8fe2008608f3cb40df88093b9f936f87fc1
ArturGimbarr75 <47144232+ArturGimbarr75@users.noreply.github.com>
2024-05-27 20:56:33 +0300

    Add User repo without realization

---
+++ DailyStatistics.Persistence/Repositories/EF/UserRepository.cs
@@ -0,0 +1,31 @@
+﻿using DailyStatistics.Persistence.Models;
+
+namespace DailyStatistics.Persistence.Repositories.EF;
+
+public class UserRepository : IUserRepository
+{
+	public Task<User> AddUserAsync(User user)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<bool> DeleteUserAsync(string userId)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<User> GetUserByEmailAsync(string email)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<User> GetUserByIdAsync(string userId)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<User> UpdateUserAsync(User user)
+	{
+		throw new NotImplementedException();
+	}
+}
