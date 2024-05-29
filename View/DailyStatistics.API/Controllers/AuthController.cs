4ea73b6cf6fba9cbc219670d01bd75f5f073eb7f
ArturGimbarr75 <123232198+ArturCodeAcademy@users.noreply.github.com>
2024-05-29 21:53:37 +0300

    Create auth controller

---
+++ View/DailyStatistics.API/Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using DailyStatistics.Application.DTO;
+using DailyStatistics.Application.Services.Errors.UserService;
+using DailyStatistics.Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DailyStatistics.API.Controllers;
+
+[ApiController]
+[Route("[controller/action]")]
+public class AuthController : Controller
+{
+	private readonly IUserService _userService;
+
+	public AuthController(IUserService userService)
+	{
+		_userService = userService;
+	}
+
+	[HttpPost("register")]
+	public async Task<ActionResult<UserDto>> Register([FromBody] UserRegistrationData registrationData)
+	{
+		var result = await _userService.CreateAsync(registrationData);
+
+		if (result)
+			return Ok(result.Value);
+
+		string[] message = result.Error switch
+		{
+			RegistrationErrors.PasswordsDoNotMatch => ["Passwords do not match"],
+			_ => result.Info ?? ["An error occurred"],
+		};
+
+		return BadRequest(message);
+	}
+}
