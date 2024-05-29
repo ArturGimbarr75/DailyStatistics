d8c7a34236cd09f9b18c23a6f9464989acad78f7
ArturGimbarr75 <123232198+ArturCodeAcademy@users.noreply.github.com>
2024-05-29 21:53:03 +0300

    Add registration helpers and errors

---
+++ DailyStatistics.Application/Helpers/RegistationHelper.cs
@@ -0,0 +1,31 @@
+﻿using DailyStatistics.Application.DTO;
+using DailyStatistics.Persistence.Models;
+
+namespace DailyStatistics.Application.Helpers;
+
+internal static class RegistationHelper
+{
+	public static User MapRegistrationData(UserRegistrationData registrationData)
+	{
+		return new User
+		{
+			Email = registrationData.Email,
+			UserName = registrationData.UserName,
+			Name = registrationData.FirstName,
+			Surname = registrationData.LastName
+		};
+	}
+
+	public static UserDto MapUserToDto(User user)
+	{
+		return new UserDto
+		{
+			Id = user.Id,
+			Email = user.Email!,
+			UserName = user.UserName!,
+			Name = user.Name,
+			Surname = user.Surname,
+			PhoneNumber = user.PhoneNumber
+		};
+	}	
+}
