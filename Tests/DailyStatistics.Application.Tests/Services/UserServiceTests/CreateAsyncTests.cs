17e0119b6a63e4afd46c0f0e2e990fa88a53fa6d
ArturGimbarr75 <47144232+ArturGimbarr75@users.noreply.github.com>
2024-05-30 18:08:34 +0300

    Add App tests project

---
+++ Tests/DailyStatistics.Application.Tests/Services/UserServiceTests/CreateAsyncTests.cs
@@ -0,0 +1,53 @@
+﻿using DailyStatistics.Application.DTO;
+using DailyStatistics.Application.Services;
+using DailyStatistics.Application.Services.Interfaces;
+using DailyStatistics.Persistence;
+using DailyStatistics.Persistence.Models;
+using DailyStatistics.Persistence.Repositories;
+using DailyStatistics.Persistence.Repositories.EF;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DailyStatistics.Application.Tests.Services.UserServiceTests;
+
+internal class CreateAsyncTests
+{
+	private IUserService _userService = default!;
+
+	[SetUp]
+	public void SetUp()
+	{
+		var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+			.UseInMemoryDatabase(Guid.NewGuid().ToString())
+			.Options;
+		var dbContext = new ApplicationDbContext(options);
+		var userStore = new UserStore<User>(dbContext);
+		var userManager = new UserManager<User>(userStore, null, null, null, null, null, null, null, null);
+		var userRepository = new UserRepository(userManager);
+
+		_userService = new UserService(userRepository);
+	}
+
+	[Test]
+	public async Task ShouldCreateUser()
+	{
+		// Arrange
+		UserRegistrationData userRegistraitionData = new()
+		{
+			UserName = "Test",
+			FirstName = "Test",
+			LastName = "Test",
+			Email = "test@gmail.com",
+			Password = "Test123",
+			ConfirmPassword = "Test123",
+			PhoneNumber = "123456789"
+		};
+
+		// Act
+		var result = _userService.CreateAsync(userRegistraitionData);
+
+		// Assert
+		Assert.IsTrue(result.Result);
+	}
+}
