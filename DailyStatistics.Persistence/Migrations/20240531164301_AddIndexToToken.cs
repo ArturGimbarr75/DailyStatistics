4e54bcdb83e939061905ad7b9111909befbf01ba
ArturGimbarr75 <47144232+ArturGimbarr75@users.noreply.github.com>
2024-05-31 19:43:41 +0300

    Add Index to token and migration

---
+++ DailyStatistics.Persistence/Migrations/20240531164301_AddIndexToToken.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DailyStatistics.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIndexToToken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Token",
+                table: "RefreshToken",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RefreshToken_Token",
+                table: "RefreshToken",
+                column: "Token");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_RefreshToken_Token",
+                table: "RefreshToken");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Token",
+                table: "RefreshToken",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
