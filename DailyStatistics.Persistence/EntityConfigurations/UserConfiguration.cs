78fadd4006fd47025f630f185be6a067cab1b045
ArturGimbarr75 <47144232+ArturGimbarr75@users.noreply.github.com>
2024-05-31 19:34:46 +0300

    Add RT model

---
+++ DailyStatistics.Persistence/EntityConfigurations/UserConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using DailyStatistics.Persistence.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DailyStatistics.Persistence.EntityConfigurations;
+
+internal class UserConfiguration : IEntityTypeConfiguration<User>
+{
+	public void Configure(EntityTypeBuilder<User> builder)
+	{
+		builder.HasMany(t => t.DayRecords)
+			.WithOne(t => t.User)
+			.HasForeignKey(t => t.UserId)
+			.OnDelete(DeleteBehavior.Cascade);
+
+		builder.HasMany(t => t.RefreshTokens)
+			.WithOne(t => t.User)
+			.HasForeignKey(t => t.UserId)
+			.OnDelete(DeleteBehavior.Cascade);
+
+		builder.HasMany(t => t.TrackingActivityGroups)
+			.WithOne(t => t.User)
+			.HasForeignKey(t => t.UserId)
+			.OnDelete(DeleteBehavior.Cascade);
+
+		builder.HasMany(t => t.TrackingActivityKinds)
+			.WithOne(t => t.User)
+			.HasForeignKey(t => t.UserId)
+			.OnDelete(DeleteBehavior.Cascade);
+	}
+}
