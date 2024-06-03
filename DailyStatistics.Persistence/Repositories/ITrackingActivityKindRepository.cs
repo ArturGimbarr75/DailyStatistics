3d81f62249ffc842de1a0abcefe61cd9f47f12af
ArturGimbarr75 <47144232+ArturGimbarr75@users.noreply.github.com>
2024-06-03 22:37:11 +0300

    Add TrackingActivityKindRepository

---
+++ DailyStatistics.Persistence/Repositories/ITrackingActivityKindRepository.cs
@@ -0,0 +1,14 @@
+﻿using DailyStatistics.Persistence.Models;
+
+namespace DailyStatistics.Persistence.Repositories;
+
+public interface ITrackingActivityKindRepository
+{
+	Task<TrackingActivityKind?> AddAsync(TrackingActivityKind trackingActivityKind);
+	Task<TrackingActivityKind?> GetAsync(Guid id);
+	Task<TrackingActivityKind?> UpdateAsync(TrackingActivityKind trackingActivityKind);
+	Task<bool> DeleteAsync(Guid id);
+	Task<IEnumerable<TrackingActivityKind>> GetAllOfUserAsync(string userId);
+	Task<bool> UserOwnsTrackingActivityKind(string userId, Guid trackingActivityKindId);
+	Task<bool> ExistsWithNamesAsync(string userId, string name);
+}
