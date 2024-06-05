8e12c2e6ee6d8fd8af77552ca05829afb9446c37
ArturGimbarr75 <47144232+ArturGimbarr75@users.noreply.github.com>
2024-06-05 23:23:36 +0300

    Add activity kinds dtos

---
+++ DailyStatistics.Application/DTO/ActivityKindCreate.cs
@@ -0,0 +1,8 @@
+﻿namespace DailyStatistics.Application.DTO;
+
+public sealed class ActivityKindCreate
+{
+	public string Name { get; set; } = string.Empty;
+	public string UserId { get; set; } = string.Empty;
+	public string? Description { get; set; }
+}
