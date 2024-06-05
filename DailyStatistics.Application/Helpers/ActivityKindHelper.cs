27de490b55076bf43fabca6f13be23f349044969
ArturGimbarr75 <47144232+ArturGimbarr75@users.noreply.github.com>
2024-06-05 23:40:35 +0300

    Create ActivityKindHelper.cs

---
+++ DailyStatistics.Application/Helpers/ActivityKindHelper.cs
@@ -0,0 +1,28 @@
+﻿using DailyStatistics.Application.DTO;
+using DailyStatistics.Persistence.Models;
+
+namespace DailyStatistics.Application.Helpers;
+
+internal static class ActivityKindHelper
+{
+	public static TrackingActivityKind MapActivityKindCreateToActivityKind(ActivityKindCreate activityKindCreate)
+	{
+		return new()
+		{
+			Name = activityKindCreate.Name,
+			UserId = activityKindCreate.UserId,
+			Description = activityKindCreate.Description
+		};
+	}
+
+	public static ActivityKindDto MapActivityKindToDto(TrackingActivityKind activityKind)
+	{
+		return new ActivityKindDto
+		{
+			Id = activityKind.Id,
+			Name = activityKind.Name,
+			UserId = activityKind.UserId,
+			Description = activityKind.Description
+		};
+	}
+}
