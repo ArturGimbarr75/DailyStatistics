0b17818a42e76f5673bcabab2fbee48e33816d65
ArturGimbarr75 <47144232+ArturGimbarr75@users.noreply.github.com>
2024-06-05 23:58:20 +0300

    Update UpdateActivityKindErrors.cs

--- DailyStatistics.Application/Services/Errors/ActivityKindService/UpdateActivityKindErrors.cs
+++ DailyStatistics.Application/Services/Errors/ActivityKindService/UpdateActivityKindErrors.cs
@@ -4,7 +4,6 @@ public enum UpdateActivityKindErrors
 {
 	InvalidJwt,
 	InvalidName,
-	InvalidUserId,
 	UserDoesNotHaveActivityKindWithThisId,
 	UserAlreadyHasActivityKindWithThisName,
 	ActivityKindNotFound
