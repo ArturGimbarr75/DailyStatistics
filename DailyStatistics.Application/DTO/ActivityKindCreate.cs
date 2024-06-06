624280baacdc7b13b0cd1de19c0acacab0d17824
ArturGimbarr75 <47144232+ArturGimbarr75@users.noreply.github.com>
2024-06-06 20:11:01 +0300

    Update ActivityKindCreate.cs

--- DailyStatistics.Application/DTO/ActivityKindCreate.cs
+++ DailyStatistics.Application/DTO/ActivityKindCreate.cs
@@ -3,6 +3,5 @@
 public sealed class ActivityKindCreate
 {
 	public string Name { get; set; } = string.Empty;
-	public string UserId { get; set; } = string.Empty;
 	public string? Description { get; set; }
 }
