edf7565ca1eef27979560a8db8338f25011b0b75
ArturGimbarr75 <47144232+ArturGimbarr75@users.noreply.github.com>
2024-06-06 21:58:43 +0300

    Add day record dtos

---
+++ DailyStatistics.Application/DTO/DayDto.cs
@@ -0,0 +1,8 @@
+﻿namespace DailyStatistics.Application.DTO;
+
+public sealed class DayDto
+{
+	public Guid Id { get; set; }
+	public DateOnly Date { get; set; }
+	public string? Description { get; set; }
+}
