fdb2c05fbe2c2e4792017d1c201c625fc5822270
ArturGimbarr75 <47144232+ArturGimbarr75@users.noreply.github.com>
2024-06-07 23:13:04 +0300

    Add TrackingActivityRecordRepository

---
+++ DailyStatistics.Persistence/Repositories/ITrackingActivityRecordRepository.cs
@@ -0,0 +1,13 @@
+﻿using DailyStatistics.Persistence.Models;
+
+namespace DailyStatistics.Persistence.Repositories;
+
+public interface ITrackingActivityRecordRepository
+{
+	Task<TrackingActivityRecord?> GetTrackingActivityRecordAsync(Guid id);
+	Task<IEnumerable<TrackingActivityRecord>> GetTrackingActivityRecordsFromDayAsync(DateOnly date, string userId);
+	Task<TrackingActivityRecord?> AddTrackingActivityRecordAsync(TrackingActivityRecord trackingActivityRecord);
+	Task<TrackingActivityRecord?> UpdateTrackingActivityRecordAsync(TrackingActivityRecord trackingActivityRecord);
+	Task<bool> DeleteTrackingActivityRecordAsync(Guid id);
+	Task<bool> DayHasRecordWithKind(DateOnly date, string userId, Guid kindId);
+}
