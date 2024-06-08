dafc6c657cdb44c10806cc8fe82aac067be4477e
ArturGimbarr75 <47144232+ArturGimbarr75@users.noreply.github.com>
2024-06-08 13:55:34 +0300

    Add service

---
+++ DailyStatistics.Application/Services/Interfaces/IActivityRecordService.cs
@@ -0,0 +1,14 @@
+﻿using DailyStatistics.Application.DTO;
+using DailyStatistics.Application.Infrastructure;
+using DailyStatistics.Application.Services.Errors.ActivityRecordService;
+
+namespace DailyStatistics.Application.Services.Interfaces;
+
+public interface IActivityRecordService
+{
+	Task<Result<ActivityRecordDto, AddRecordErrors>> AddActivityRecordAsync(ActivityRecordDto activityRecordDto, string userId);
+	Task<Result<ActivityRecordDto, UpdateRecordErrors>> UpdateActivityRecordAsync(ActivityRecordDto activityRecordDto, string userId);
+	Task<Result<DeleteRecordErrors>> DeleteActivityRecordAsync(Guid id, string userId);
+	Task<Result<ActivityRecordDto, GetRecordErrors>> GetActivityRecordAsync(Guid id, string userId);
+	Task<Result<IEnumerable<ActivityRecordDto>, GetRecordErrors>> GetActivityRecordsFromDayAsync(DateOnly date, string userId);
+}
