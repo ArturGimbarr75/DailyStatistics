1acdc5040f21a73a213a28cfcaf36b896476f976
ArturGimbarr75 <47144232+ArturGimbarr75@users.noreply.github.com>
2024-06-10 23:34:23 +0300

    Add image repo

---
+++ DailyStatistics.Persistence/Repositories/IProfileImageRepository.cs
@@ -0,0 +1,14 @@
+﻿using DailyStatistics.Persistence.Models;
+
+namespace DailyStatistics.Persistence.Repositories;
+
+public interface IProfileImageRepository
+{
+	Task<ProfileImage?> GetSelectedProfileImage(string userId);
+	Task<ProfileImage?> GetProfileImageByIdAsync(string profileImageId);
+	Task<ProfileImage?> GetProfileImageByUserIdAsync(string userId);
+	Task<IEnumerable<ProfileImage>> GetProfileImagesByUserIdAsync(string userId);
+	Task<ProfileImage?> AddProfileImageAsync(ProfileImage profileImage);
+	Task<bool> DeleteProfileImageAsync(string profileImageId);
+	Task DeleteAllUserProfileImagesAsync(string userId);
+}
