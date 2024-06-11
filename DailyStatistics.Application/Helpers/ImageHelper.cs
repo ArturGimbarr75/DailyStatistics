6e7cec973ce2df7157e9e69c617df81f90de7966
ArturGimbarr75 <47144232+ArturGimbarr75@users.noreply.github.com>
2024-06-11 22:08:16 +0300

    Add image helpers

---
+++ DailyStatistics.Application/Helpers/ImageHelper.cs
@@ -0,0 +1,28 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+
+namespace DailyStatistics.Application.Helpers;
+
+internal static class ImageHelper
+{
+	internal static bool IsValidImageExtension(string extension, string[] allowedExtensions)
+	{
+		return allowedExtensions.Contains(extension);
+	}
+
+	internal static void CreateThumbnail(int lnWidth, int lnHeight, byte[] bytes, string outputFilename)
+	{
+		using (Image image = Image.Load(bytes))
+		{
+			var ratioX = (double)lnWidth / image.Width;
+			var ratioY = (double)lnHeight / image.Height;
+			var ratio = Math.Min(ratioX, ratioY);
+
+			var newWidth = (int)(image.Width * ratio);
+			var newHeight = (int)(image.Height * ratio);
+
+			image.Mutate(x => x.Resize(newWidth, newHeight));
+			image.Save(outputFilename);
+		}
+	}
+}
---
+++ DailyStatistics.Application/Helpers/ProfileImageHelper.cs
@@ -0,0 +1,26 @@
+﻿using DailyStatistics.Application.DTO;
+using DailyStatistics.Persistence.Models;
+
+namespace DailyStatistics.Application.Helpers;
+
+internal static class ProfileImageHelper
+{
+	internal static ImageDto MapProfileImageToDto(ProfileImage profileImage)
+	{
+		return new ImageDto
+		{
+			Id = profileImage.Id,
+			UserId = profileImage.UserId!
+		};
+	}
+    
+	internal static ProfileImage MapProfileImageDtoToProfileImage(ImageDto imageDto, string path)
+	{
+		return new ProfileImage
+		{
+			Id = imageDto.Id,
+			UserId = imageDto.UserId,
+			ImagePath = path
+		};
+	}
+}
