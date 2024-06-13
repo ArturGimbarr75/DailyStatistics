e4a61a2cb44997f6a5d586ece78b2e3b806fc003
ArturGimbarr75 <47144232+ArturGimbarr75@users.noreply.github.com>
2024-06-13 15:35:46 +0300

    Create ActivityRecordCreate.cs

---
+++ DailyStatistics.Application/DTO/ActivityRecordCreate.cs
@@ -0,0 +1,8 @@
+﻿namespace DailyStatistics.Application.DTO;
+
+public sealed class ActivityRecordCreate
+{
+	public Guid DayRecordId { get; set; }
+	public Guid ActivityKindId { get; set; }
+	public double Amount { get; set; }
+}
