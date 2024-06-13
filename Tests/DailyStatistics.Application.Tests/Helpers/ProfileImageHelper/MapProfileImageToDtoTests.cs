c66f7001467aa752647092d977aeb26ae8aef7be
ArturGimbarr75 <47144232+ArturGimbarr75@users.noreply.github.com>
2024-06-13 20:26:55 +0300

    Add ProfileImageHelper tests

---
+++ Tests/DailyStatistics.Application.Tests/Helpers/ProfileImageHelper/MapProfileImageToDtoTests.cs
@@ -0,0 +1,36 @@
+﻿using DailyStatistics.Persistence.Models;
+using Helper = DailyStatistics.Application.Helpers.ProfileImageHelper;
+
+namespace DailyStatistics.Application.Tests.Helpers.ProfileImageHelper;
+
+internal class MapProfileImageToDtoTests
+{
+	[Test]
+	public void ShouldMapProfileImageToDto()
+	{
+		// Arrange
+		var profileImage = new ProfileImage()
+		{
+			Id = Guid.NewGuid(),
+			UserId = "1"
+		};
+
+		// Act
+		var result = Helper.MapProfileImageToDto(profileImage);
+
+		// Assert
+		Assert.That(result, Is.Not.Null);
+		Assert.That(result.Id, Is.EqualTo(profileImage.Id));
+		Assert.That(result.UserId, Is.EqualTo(profileImage.UserId));
+	}
+
+	[Test]
+	public void ShouldThrowNullReferenceException_WhenProfileImageIsNull()
+	{
+		// Arrange
+		ProfileImage? profileImage = null;
+
+		// Act & Assert
+		Assert.Throws<NullReferenceException>(() => Helper.MapProfileImageToDto(profileImage!));
+	}
+}
